85a3ca778b628073f0bb5dab135dd634eafd79bc
dejun <hengdj.22@ichat.sp.edu.sg>
2024-11-20 14:58:35 +0800

    with return values for unit testing

--- bmi.py
+++ bmi.py
@@ -8,9 +8,12 @@ def calculate_bmi(height,weight):
 
     if(bmi < 18.5):
         print("Under weight")
+        return -1
     elif(18.5<=bmi<=25.0):
         print("Normal weight")
+        return 0
     elif(bmi>25.0):
         print("Over weight")
+        return 1
 
 calculate_bmi(weight=57, height=1.73)
